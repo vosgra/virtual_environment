ae76398669d07a8268c58eb24fcb4fc17b816ee7
Stamatis Asvestas <asvestasstamatis@gmail.com>
2024-10-28 18:14:23 +0200

    mushrooms
    Mushrooms Added , colliders added , script added, press e to take mushroom

---
+++ King_Assasin/Assets/ManitariaCollider.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace Unity.FantasyKingdom
+{
+    public class ManitariaCollider : MonoBehaviour
+    {
+        private bool isPlayerInRange = false;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isPlayerInRange && Input.GetKeyDown(KeyCode.E))
+            {
+                // Make the object disappear
+                gameObject.SetActive(false);
+            }
+        }
+
+        private void OnTriggerEnter(Collider other) {
+
+            if (other.CompareTag("Player"))
+            {
+                isPlayerInRange = true;
+                
+            }
+
+        }
+        private void OnTriggerStay(Collider other)
+        {
+
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                isPlayerInRange = false;
+            }
+        }
+    }
+}
