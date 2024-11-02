f1c8d78c0169d88cc83eb9226ea1289f96caebd0
Stamatis Asvestas <asvestasstamatis@gmail.com>
2024-11-02 02:29:17 +0200

    animation try

--- King_Assasin/Assets/ManitariaCollider.cs
+++ King_Assasin/Assets/ManitariaCollider.cs
@@ -5,14 +5,12 @@ namespace Unity.FantasyKingdom
     public class ManitariaCollider : MonoBehaviour
     {
         public GameObject textObject;
-        public GameObject textObject1;
-        public GameObject textObject2;
-        public GameObject textObject3;
+        
         private bool isPlayerInRange = false;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-        
+             
         }
 
         // Update is called once per frame
@@ -22,24 +20,20 @@ namespace Unity.FantasyKingdom
             {
                 // Make the object disappear
                 gameObject.SetActive(false);
-
+                
             }
             if (isPlayerInRange)
             {
 
                 textObject.SetActive(true);
-                textObject3.SetActive(true);
-                textObject2.SetActive(true);
-                textObject1.SetActive(true);
+                
 
 
             }
             else
             {
                 textObject.SetActive(false);
-                textObject3.SetActive(false);
-                textObject2.SetActive(false);
-                textObject1.SetActive(false);
+                
             }
         }
         private void OnTriggerEnter(Collider other) {
